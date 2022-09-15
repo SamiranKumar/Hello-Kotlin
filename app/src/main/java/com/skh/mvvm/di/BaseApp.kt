44bd422ab51fe134cb328b5e31eddc4b78067486
SamiranDevo <samirankumar11@gmail.com>
2022-09-15 19:58:50 +0600

    Add BaseApp Application class with Hilt inject

---
+++ app/src/main/java/com/skh/mvvm/di/BaseApp.kt
@@ -0,0 +1,12 @@
+package com.skh.mvvm.di
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+
+/**
+Created by Samiran Kumar on 20,September,2022
+ **/
+@HiltAndroidApp
+class BaseApp : Application() {
+}
